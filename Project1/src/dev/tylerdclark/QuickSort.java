891da5a7b734d8ace89c9309b37385d123dfc0dc
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-09-09 23:38:35 -0400

    implementing the interface

--- Project1/src/dev/tylerdclark/QuickSort.java
+++ Project1/src/dev/tylerdclark/QuickSort.java
@@ -1,2 +1,24 @@
-package dev.tylerdclark;public class QuickSort {
+package dev.tylerdclark;
+
+public class QuickSort implements SortInterface{
+    
+    @Override
+    public void recursiveSort(int[] list) throws UnsortedException {
+
+    }
+
+    @Override
+    public void iterativeSort(int[] list) throws UnsortedException {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public long getTime() {
+        return 0;
+    }
 }
